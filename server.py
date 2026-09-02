79ab937be467583ed252bd3f3eb03ccb65f37a45
jyagain <privilegedjy@gmail.com>
2026-09-03 00:08:45 +0900

    chore: add deployment config files (requirements.txt, Procfile) and update server PORT binding

--- server.py
+++ server.py
@@ -220,4 +220,6 @@ async def send_test_notification():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
+
